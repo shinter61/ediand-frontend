956e4f40e535336c55ba8efd8bb6fedf924c31fe
shinter61 <shinter61@gmail.com>
2020-09-06 03:24:57 +0900

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>hello, world!!</div>
+    );
+  }
+}
+
+export default App;
