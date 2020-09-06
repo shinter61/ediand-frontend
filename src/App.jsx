485f026b7601da43344f430ed8c5aaea2000a6fa
shinter61 <shinter61@gmail.com>
2020-09-07 01:10:08 +0900

    Only master

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>Goodbye, cruel world.</div>
+      <div>Goodbye, cruel world!</div>
     );
   }
 }
