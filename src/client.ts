c1f6ac2ab31b3ecdd16b1e92c54f1a3999c6fa04
shinter61 <shinter61@gmail.com>
2020-12-14 02:46:56 +0900

    Directly assign production url

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: process.env.API_ENDPOINT,
+  // baseURL: process.env.API_ENDPOINT,
+  baseURL: "http://18.181.8.233",
   timeout: 3 * 60 * 1000,
 });
 
